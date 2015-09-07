f58544bb17c424d5f6f4bfc127594e7d53f9d0ab
Jere Hupanen <jere.hupanen@cs.helsinki.fi>
2015-09-07 22:04:41 +0300

    rikottu testi

--- OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -95,7 +95,7 @@ public class VarastoTest {
         double vanhasaldo = varasto.getSaldo();
         varasto.otaVarastosta(-1);
         
-        assertEquals(vanhasaldo, varasto.getSaldo(), vertailuTarkkuus);
+        assertEquals(-1, varasto.getSaldo(), vertailuTarkkuus); //replace -1 with vanhasaldo
     }
     
     @Test
